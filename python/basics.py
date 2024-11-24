27a62c7ea3de6b122de01d58d38640c31b343043
maryam-m1 <maryamrajput0013@gmail.com>
2024-11-24 20:45:25 +0500

    added basic.py

--- python/basics.py
+++ python/basics.py
@@ -1,1 +1,8 @@
-print("Burger in Lahore")
+#Calculator 
+x=input("Please enter first Number : ")
+y=input("Please enter second Number : ")
+print("Adiition of two numbers: ",int(x)+int(y))
+print("Subtaction of two numbers: ",int(x)-int(y))
+print("Multiplication of two numbers: ",int(x)*int(y))
+print("Floor Division of two numbers: ",int(x)/int(y))
+print("Moudular Division of two numbers: ",int(x)%int(y))
