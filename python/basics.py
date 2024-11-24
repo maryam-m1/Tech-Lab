a9185a51f6006c429e96e9be89b3b492a6a9c71e
maryam-m1 <maryamrajput0013@gmail.com>
2024-11-24 22:14:51 +0500

    added basics.py

--- python/basics.py
+++ python/basics.py
@@ -10,5 +10,17 @@ while (y<0):
 print("Adiition of two numbers: ",x+y)
 print("Subtaction of two numbers: ",x-y)
 print("Multiplication of two numbers: ",x*y)
-print("Floor Division of two numbers: ",x//y)
-print("Moudular Division of two numbers: ", x%y ) 
+if(x<=0):
+    m=int(input("Enter the first digit: "))
+    while(m<=0):
+        m=int(input("Enter the first digit: "))
+else:
+    m=x;    
+if(x<=0):
+    n=int(input("Enter the first digit: "))
+    while(m<=0):
+        m=int(input("Enter the first digit: "))
+else:
+    n=x; 
+print("Floor Division of two numbers: ",m//n)
+print("Moudular Division of two numbers: ", m%n ) 
