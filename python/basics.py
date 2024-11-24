980537f894a37d895c9d5865c1ba00404782894c
maryam-m1 <maryamrajput0013@gmail.com>
2024-11-24 22:03:49 +0500

    added basics.py

--- python/basics.py
+++ python/basics.py
@@ -1,8 +1,14 @@
 #Calculator 
-x=input("Please enter first Number : ")
-y=input("Please enter second Number : ")
-print("Adiition of two numbers: ",int(x)+int(y))
-print("Subtaction of two numbers: ",int(x)-int(y))
-print("Multiplication of two numbers: ",int(x)*int(y))
-print("Floor Division of two numbers: ",int(x)/int(y))
-print("Moudular Division of two numbers: ",int(x)%int(y))
+x=int(input("Please enter first Number : "))
+while (x<=0):
+  x=int(input("Please enter first Number : "))    
+
+y=int(input("Please enter second Number : "))
+while (y<=0):
+      y=int(input("Please enter second Number : ")) 
+
+print("Adiition of two numbers: ",x+y)
+print("Subtaction of two numbers: ",x-y)
+print("Multiplication of two numbers: ",x*y)
+print("Floor Division of two numbers: ",x//y)
+print("Moudular Division of two numbers: ", x%y ) 
